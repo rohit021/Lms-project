46c0d06f5149016090d54408388ebcda4c962874
prateek1998 <37973089+prateek1998@users.noreply.github.com>
2021-04-06 16:46:35 +0530

    Update MainRouter.js

--- client/src/MainRouter.js
+++ client/src/MainRouter.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
-import Login from "./pages/authentication/login/login";
-import Register from "./pages/authentication/register/register";
+import Login from "./pages/authentication/login/Login";
+import Register from "./pages/authentication/register/Register";
 import ResetPass from "./pages/authentication/resetpass";
 import NewPass from "./pages/authentication/newpass";
 import PrivateRoute from "./components/routing/PrivateRoute";
